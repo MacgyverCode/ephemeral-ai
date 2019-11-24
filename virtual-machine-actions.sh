b9085089bad34aca6918c0ba19f05b2ec9fbf0e3
Tim Moody <timothydmoody@gmail.com>
2019-11-24 15:26:45 -0500

    Create virtual-machine-actions.sh

---
+++ virtual-machine-actions.sh
@@ -0,0 +1,6 @@
+# Start the Virtual Machine
+gcloud compute instances start whimsical-8 --zone us-central1-c
+
+
+# Stop the Virtual Machine
+gcloud compute instances stop whimsical-8 --zone us-central1-c
